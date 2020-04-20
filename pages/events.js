4a5b691419bdb419550fda36529385d7b49bfd9f
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-21 00:57:52 +0530

    Made the events dynamic to fetch the data

--- pages/events.js
+++ pages/events.js
@@ -3,14 +3,25 @@ import Navbar from '../components/Layouts/Navbar';
 import Footer from '../components/Layouts/Footer';
 import Banner from '../components/Events/Banner';
 import Content from '../components/Events/Content';
+import axios from "../config/axios";
 
 class Events extends Component {
+    static async getInitialProps(ctx) {
+        const res = await axios(
+            {
+                url: '/events',
+                method: 'GET'
+            }
+        )
+        const eventsArray = await res.data
+        return {events: eventsArray}
+    }
     render() {
         return (
             <>
                 <Navbar />
                 <Banner />
-                <Content />
+                <Content events={this.props.events} />
                 <Footer />
             </>
         );
