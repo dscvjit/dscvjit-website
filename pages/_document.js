b2fb93a095224f1b66e2deac851dc43dddaf3174
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-20 14:23:36 +0530

    Modified the font css

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import favicon from '../images/favicon.png'
+import React from "react";
 class MyDocument extends Document {
 
     render() {
@@ -7,8 +8,7 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                    <link rel="icon" type="image/png" href={require("../images/favicon.png")}/>
-                    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+                    <link rel="icon" type="image/png" href={favicon}/>
                 </Head>
                 <body>
                     <Main />
