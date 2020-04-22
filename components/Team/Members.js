d4894c6fa529cc0f2b4568e4db6d0544bb1ffd13
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-22 18:01:15 +0530

    Made the team dynamic to fetch the details from the backend

--- components/Team/Members.js
+++ components/Team/Members.js
@@ -1,326 +1,56 @@
-import React, { Component } from 'react';
-import Link from 'next/link';
-import dscLogoProject from '../../images/dsc-logo-project.png'
-import ashfaqImage from '../../images/team/ashfaq.jpeg'
+import React from 'react';
 
-class Members extends Component {
-    render() {
-        return (
-            <section className="team-area ptb-100">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-6">
+const Members = ({members}) => {
+    return (
+        <section className="team-area ptb-100">
+            <div className="container">
+                <div className="row">
+                    {members.map(member => (
+                        <div className="col-lg-3 col-md-6" key={member.id}>
                             <div className="single-team-member">
-                                <img src={ashfaqImage} alt="team" />
+                                <img src={member.image} alt="team"/>
 
                                 <div className="team-content">
-                                    <h3>Ashfaq Nisar</h3>
-                                    <span>DSC Lead 2019</span>
+                                    <h3>{member.name}</h3>
+                                    <span>{member.designation}</span>
 
                                     <ul>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-facebook"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-instagram"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-twitter"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-linkedin"></i></a>
-                                            </Link>
-                                        </li>
+                                        {member.socialLinks.github !== "" ? (
+                                            <li>
+                                                <a href={member.socialLinks.github} target={"_blank"}><i className="icofont-github"/></a>
+                                            </li>) : <></>
+                                        }
+                                        {member.socialLinks.facebook !== "" ? (
+                                            <li>
+                                                <a href={member.socialLinks.facebook} target={"_blank"}><i
+                                                    className="icofont-facebook"/></a>
+                                            </li>) : <></>
+                                        }
+
+                                        {member.socialLinks.linkedin !== "" ? (
+                                            <li>
+                                                <a href={member.socialLinks.linkedin} target={"_blank"}><i className="icofont-linkedin"/></a>
+                                            </li>) : <></>
+                                        }
+                                        {member.socialLinks.twitter !== "" ? (
+                                            <li>
+                                                <a href={member.socialLinks.twitter} target={"_blank"}><i
+                                                    className="icofont-twitter"/></a>
+                                            </li>) : <></>
+                                        }
+
                                     </ul>
                                 </div>
                             </div>
                         </div>
-
-                        <div className="col-lg-3 col-md-6">
-                            <div className="single-team-member">
-                                <img src={dscLogoProject} alt="team" />
-
-                                <div className="team-content">
-                                    <h3>Manasa</h3>
-                                    <span>Title</span>
-
-                                    <ul>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-facebook"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-instagram"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-twitter"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-linkedin"></i></a>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6">
-                            <div className="single-team-member">
-                                <img src={dscLogoProject} alt="team" />
-
-                                <div className="team-content">
-                                    <h3>Vinay Reddy</h3>
-                                    <span>Title</span>
-
-                                    <ul>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-facebook"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-instagram"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-twitter"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-linkedin"></i></a>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6">
-                            <div className="single-team-member">
-                                <img src={dscLogoProject} alt="team" />
-
-                                <div className="team-content">
-                                    <h3>Manogna Rayasam</h3>
-                                    <span>Title</span>
-
-                                    <ul>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-facebook"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-instagram"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-twitter"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-linkedin"></i></a>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6">
-                            <div className="single-team-member">
-                                <img src={dscLogoProject} alt="team" />
-
-                                <div className="team-content">
-                                    <h3>Vamshi Krishna</h3>
-                                    <span>Title</span>
-
-                                    <ul>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-facebook"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-instagram"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-twitter"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-linkedin"></i></a>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6">
-                            <div className="single-team-member">
-                                <img src={dscLogoProject} alt="team" />
-
-                                <div className="team-content">
-                                    <h3>Vikas Bandaru</h3>
-                                    <span>Title</span>
-
-                                    <ul>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-facebook"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-instagram"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-twitter"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-linkedin"></i></a>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6">
-                            <div className="single-team-member">
-                                <img src={dscLogoProject} alt="team" />
-
-                                <div className="team-content">
-                                    <h3>Naveen Nani</h3>
-                                    <span>Title</span>
-
-                                    <ul>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-facebook"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-instagram"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-twitter"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-linkedin"></i></a>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6">
-                            <div className="single-team-member">
-                                <img src={dscLogoProject} alt="team" />
-
-                                <div className="team-content">
-                                    <h3>Sharan Patel</h3>
-                                    <span>Title</span>
-
-                                    <ul>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-facebook"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-instagram"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-twitter"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-linkedin"></i></a>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6">
-                            <div className="single-team-member">
-                                <img src={dscLogoProject} alt="team" />
-
-                                <div className="team-content">
-                                    <h3>Venkatesh</h3>
-                                    <span>Title</span>
-
-                                    <ul>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-facebook"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-instagram"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-twitter"></i></a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">
-                                                <a><i className="icofont-linkedin"></i></a>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
+                    ))}
 
 
-                    </div>
                 </div>
-            </section>
-        );
-    }
+            </div>
+        </section>
+    );
+
 }
 
 export default Members;
