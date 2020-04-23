76abc3e93e63178ec52ff45b8a6547f15ac99601
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-23 22:19:29 +0530

    Fixed the issue with the event details page

--- components/EventDetails/DetailsContent.js
+++ components/EventDetails/DetailsContent.js
@@ -12,7 +12,9 @@ const DetailsContent = ({ event }) => {
             </div>
           </div>
         </div>
-        <div className={'container'}>
+      </div>
+      <div className={'container'}>
+        <div className="row">
           <div className="col-lg-12 col-md-12">
             <div className="project-details-desc">
               <h2>{event.name}</h2>
