920073fa979dde5b46c03eec79758890b69483d0
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-23 22:09:29 +0530

    Conversion from CSS to SCSS

--- components/EventDetails/DetailsContent.js
+++ components/EventDetails/DetailsContent.js
@@ -1,55 +1,61 @@
 import React from 'react';
+import { Chip } from '@material-ui/core';
 
 const DetailsContent = ({ event }) => {
   return (
     <section className="project-details-area ptb-100">
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
           <div className="col-lg-12 col-md-12">
             <div className="project-details-image">
               <img src={event.image} alt="work" />
             </div>
           </div>
-
+        </div>
+        <div className={'container'}>
           <div className="col-lg-12 col-md-12">
             <div className="project-details-desc">
               <h2>{event.name}</h2>
-              <p>{event.des}</p>
+            </div>
+            <div>
+              <div className="project-details-desc">
+                <p>{event.des}</p>
 
-              <div className="project-details-information">
-                <div className="single-info-box">
-                  <h4>Conducted By</h4>
-                  <p>DSC Web Team</p>
-                </div>
-
-                <div className="single-info-box">
-                  <h4>Category</h4>
-                  <p>Workshop</p>
-                </div>
-
-                <div className="single-info-box">
-                  <h4>Conducted On</h4>
-                  <p>19-04-2020</p>
-                </div>
-
-                <div className="single-info-box">
-                  <h4>Attendees</h4>
-                  <p>70</p>
-                </div>
-
-                {event.links.registration !== '' ? (
+                <div className="project-details-information">
                   <div className="single-info-box">
-                    <a
-                      href={event.links.registration}
-                      target={'_blank'}
-                      className="btn btn-primary"
-                    >
-                      Register Now
-                    </a>
+                    <h4>Conducted By</h4>
+                    <p>DSC Web Team</p>
                   </div>
-                ) : (
-                  <></>
-                )}
+
+                  <div className="single-info-box">
+                    <h4>Category</h4>
+                    <p>Workshop</p>
+                  </div>
+
+                  <div className="single-info-box">
+                    <h4>Conducted On</h4>
+                    <p>19-04-2020</p>
+                  </div>
+
+                  <div className="single-info-box">
+                    <h4>Attendees</h4>
+                    <p>70</p>
+                  </div>
+
+                  {event.links.registration !== '' ? (
+                    <div className="single-info-box">
+                      <a
+                        href={event.links.registration}
+                        target={'_blank'}
+                        className="btn btn-primary"
+                      >
+                        Register Now
+                      </a>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
               </div>
             </div>
           </div>
