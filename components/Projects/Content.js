98f59b111d2354fad9ce33b0b5cb87929435a589
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-24 20:47:34 +0530

    Removed the pagination from the pages

--- components/Projects/Content.js
+++ components/Projects/Content.js
@@ -31,44 +31,6 @@ const Content = ({ projects }) => {
               </div>
             </div>
           ))}
-
-          <div className="col-lg-12 col-md-12">
-            <div className="pagination-area">
-              <nav aria-label="Page navigation example">
-                <ul className="pagination justify-content-center">
-                  <li className="page-item">
-                    <a className="page-link" href="#">
-                      <i className="icofont-double-left" />
-                    </a>
-                  </li>
-
-                  <li className="page-item active">
-                    <a className="page-link" href="#">
-                      1
-                    </a>
-                  </li>
-
-                  <li className="page-item">
-                    <a className="page-link" href="#">
-                      2
-                    </a>
-                  </li>
-
-                  <li className="page-item">
-                    <a className="page-link" href="#">
-                      3
-                    </a>
-                  </li>
-
-                  <li className="page-item">
-                    <a className="page-link" href="#">
-                      <i className="icofont-double-right" />
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
         </div>
       </div>
     </section>
