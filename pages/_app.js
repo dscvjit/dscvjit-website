ed9d64cce3d2a530b4b6a1f3d9ebdb7bd2fcdb1e
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-27 21:07:07 +0530

    Removed the redux

--- pages/_app.js
+++ pages/_app.js
@@ -9,42 +9,37 @@ import '../assets/css/animate.css';
 import { Provider } from 'react-redux';
 import { Preloader, Placeholder } from 'react-preloading-screen';
 import App from 'next/app';
-import withRedux from 'next-redux-wrapper';
-import { initStore } from '../store/store';
 import { DefaultSeo } from 'next-seo';
 import GoTop from '../components/Shared/GoTop';
 import Loader from '../components/Shared/Loader';
 import React from 'react';
 import '../fontAwesome';
 
-export default withRedux(initStore)(
-  class MyApp extends App {
-    render() {
-      const { Component, pageProps, store } = this.props;
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, store } = this.props;
 
-      return (
-        <>
-          <DefaultSeo
-            title="Developer Student Clubs VJIT"
-            description="Developer Student Clubs VJIT"
-            openGraph={{
-              type: 'website',
-              locale: 'en_IE',
-              url: 'https://dscvjit.com',
-              site_name: 'DSC VJIT'
-            }}
-          />
-          <Preloader>
-            <Provider store={store}>
-              <Component {...pageProps} />
-            </Provider>
-            <GoTop scrollStepInPx="50" delayInMs="20" />
-            <Placeholder>
-              <Loader />
-            </Placeholder>
-          </Preloader>
-        </>
-      );
-    }
+    return (
+      <>
+        <DefaultSeo
+          title="Developer Student Clubs VJIT"
+          description="The Developer Student Clubs website for Vidya Jyothi Institute of Technology"
+          openGraph={{
+            type: 'website',
+            locale: 'en_IE',
+            url: 'https://dscvjit.com',
+            site_name: 'DSC VJIT'
+          }}
+        />
+        <Preloader>
+          <Component {...pageProps} />
+          <GoTop scrollStepInPx="50" delayInMs="20" />
+          <Placeholder>
+            <Loader />
+          </Placeholder>
+        </Preloader>
+      </>
+    );
   }
-);
+}
+export default MyApp;
