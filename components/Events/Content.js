2cd33fd11f00389494af3a2b16763762008100d1
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-29 22:31:32 +0530

    Modified the events/projects page to handle no data

--- components/Events/Content.js
+++ components/Events/Content.js
@@ -15,79 +15,79 @@ const Content = (props) => {
     <section className="project-area ptb-100">
       <div className="container">
         <div className="row">
-          {!events
-            ? skeletonArray.map((value, index) => (
-                <div className="col-lg-6 col-md-6" key={index}>
-                  <div className="single-project">
-                    <div className="project-image">
-                      <Skeleton
-                        className={'event'}
-                        animation="wave"
-                        variant={'rect'}
-                        width="100%"
-                      />
-                    </div>
+          {!events ? (
+            skeletonArray.map((value, index) => (
+              <div className="col-lg-6 col-md-6" key={index}>
+                <div className="single-project">
+                  <div className="project-image">
+                    <Skeleton
+                      className={'event'}
+                      animation="wave"
+                      variant={'rect'}
+                      width="100%"
+                    />
+                  </div>
 
-                    <div className="project-content">
-                      <span className="category">
-                        <Skeleton animation="wave" width="60%" />
-                      </span>
-                      <Skeleton animation="wave" variant={'text'} />
-                    </div>
+                  <div className="project-content">
+                    <span className="category">
+                      <Skeleton animation="wave" width="60%" />
+                    </span>
+                    <Skeleton animation="wave" variant={'text'} />
                   </div>
                 </div>
-              ))
-            : events.data.map((event) => (
-                <div className="col-lg-6 col-md-6" key={event.id}>
-                  <div
-                    className="single-project"
-                    style={{ cursor: 'pointer' }}
-                    onClick={() => {
-                      Router.push(`/events/${event.id}`);
-                    }}
-                  >
-                    <div className="project-image">
-                      <img
-                        className="event"
-                        src={event.image}
-                        alt={event.name}
-                      />
-                    </div>
+              </div>
+            ))
+          ) : events['success'] ? (
+            events.data.map((event) => (
+              <div className="col-lg-6 col-md-6" key={event.id}>
+                <div
+                  className="single-project"
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => {
+                    Router.push(`/events/${event.id}`);
+                  }}
+                >
+                  <div className="project-image">
+                    <img className="event" src={event.image} alt={event.name} />
+                  </div>
 
-                    <div className="project-content">
-                      <span className="category">
-                        Conducted At {event.date}
-                      </span>
-                      <Grid
-                        container
-                        alignItems={'center'}
-                        justify={'space-between'}
-                      >
-                        <Grid item>
-                          <h3>
-                            <Link href={`/events/${event.id}`}>
-                              <a>{event.name}</a>
-                            </Link>
-                          </h3>
-                        </Grid>
-                        <Grid item>
-                          {event.active ? (
-                            <Chip
-                              className={'status-chip active'}
-                              label={'Active'}
-                            />
-                          ) : (
-                            <Chip
-                              className={'status-chip completed'}
-                              label={'Completed'}
-                            />
-                          )}
-                        </Grid>
+                  <div className="project-content">
+                    <span className="category">Conducted At {event.date}</span>
+                    <Grid
+                      container
+                      alignItems={'center'}
+                      justify={'space-between'}
+                    >
+                      <Grid item>
+                        <h3>
+                          <Link href={`/events/${event.id}`}>
+                            <a>{event.name}</a>
+                          </Link>
+                        </h3>
                       </Grid>
-                    </div>
+                      <Grid item>
+                        {event.active ? (
+                          <Chip
+                            className={'status-chip active'}
+                            label={'Active'}
+                          />
+                        ) : (
+                          <Chip
+                            className={'status-chip completed'}
+                            label={'Completed'}
+                          />
+                        )}
+                      </Grid>
+                    </Grid>
                   </div>
                 </div>
-              ))}
+              </div>
+            ))
+          ) : (
+            <h4 style={{ marginRight: 'auto', marginLeft: 'auto' }}>
+              No events are available right now
+            </h4>
+          )}
         </div>
       </div>
     </section>
