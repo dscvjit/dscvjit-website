073ba78c492c24aadbe94be027aa33d57386b0bf
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-29 22:31:32 +0530

    Modified the events/projects page to handle no data

--- components/Projects/Content.js
+++ components/Projects/Content.js
@@ -16,54 +16,60 @@ const Content = (props) => {
     <section className="project-area ptb-100">
       <div className="container">
         <div className="row">
-          {!projects
-            ? skeletonArray.map((value, index) => (
-                <div className="col-lg-4 col-md-6" key={index}>
-                  <div className="single-project">
-                    <div className="project-image">
-                      <Skeleton
-                        className={'project'}
-                        animation="wave"
-                        variant={'rect'}
-                        width="100%"
-                      />
-                    </div>
+          {!projects ? (
+            skeletonArray.map((value, index) => (
+              <div className="col-lg-4 col-md-6" key={index}>
+                <div className="single-project">
+                  <div className="project-image">
+                    <Skeleton
+                      className={'project'}
+                      animation="wave"
+                      variant={'rect'}
+                      width="100%"
+                    />
+                  </div>
 
-                    <div className="project-content">
-                      <Skeleton animation="wave" width="60%" />
-                      <Skeleton animation="wave" variant={'text'} />
-                    </div>
+                  <div className="project-content">
+                    <Skeleton animation="wave" width="60%" />
+                    <Skeleton animation="wave" variant={'text'} />
                   </div>
                 </div>
-              ))
-            : projects.data.map((project) => (
-                <div className="col-lg-4 col-md-6" key={project.id}>
-                  <div
-                    className="single-project"
-                    style={{ cursor: 'pointer' }}
-                    onClick={() => {
-                      Router.push(`/projects/${project.id}`);
-                    }}
-                  >
-                    <div className="project-image">
-                      <img
-                        className="project"
-                        src={project.image}
-                        alt={project.name}
-                      />
-                    </div>
+              </div>
+            ))
+          ) : projects['success'] ? (
+            projects.data.map((project) => (
+              <div className="col-lg-4 col-md-6" key={project.id}>
+                <div
+                  className="single-project"
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => {
+                    Router.push(`/projects/${project.id}`);
+                  }}
+                >
+                  <div className="project-image">
+                    <img
+                      className="project"
+                      src={project.image}
+                      alt={project.name}
+                    />
+                  </div>
 
-                    <div className="project-content">
-                      <span className="category">{project.category}</span>
-                      <h3>
-                        <Link href={`/projects/${project.id}`}>
-                          <a>{project.name}</a>
-                        </Link>
-                      </h3>
-                    </div>
+                  <div className="project-content">
+                    <span className="category">{project.category}</span>
+                    <h3>
+                      <Link href={`/projects/${project.id}`}>
+                        <a>{project.name}</a>
+                      </Link>
+                    </h3>
                   </div>
                 </div>
-              ))}
+              </div>
+            ))
+          ) : (
+            <h4 style={{ marginRight: 'auto', marginLeft: 'auto' }}>
+              No projects are available right now
+            </h4>
+          )}
         </div>
       </div>
     </section>
