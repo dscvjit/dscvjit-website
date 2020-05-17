a8488b417ebd762fd130a31a4f1c231c39c4bda3
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-05-17 19:48:58 +0530

    Solved all the issues with the website

--- components/ContactUs/Banner.js
+++ components/ContactUs/Banner.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Banner extends Component {
-  render() {
-    return (
-      <section className="page-title-banner">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <h2>Contact Us</h2>
-            </div>
+const Banner = () => {
+  return (
+    <section className="page-title-banner" style={{ paddingBottom: '0px' }}>
+      <div className="container">
+        <div className="row">
+          <div className="section-title">
+            <h2>Lets Talk !</h2>
+            <div className="bar" />
+            <p>Would like to sponsor an event. Get in touch with us!</p>
           </div>
         </div>
+      </div>
 
-        <div className="shape1">
-          <img src={require('../../images/shape1.png')} alt="img" />
-        </div>
-        <div className="shape2">
-          <img src={require('../../images/shape2.png')} alt="img" />
-        </div>
-        <div className="shape3">
-          <img src={require('../../images/shape3.png')} alt="img" />
-        </div>
-        <div className="shape6">
-          <img src={require('../../images/shape6.png')} alt="img" />
-        </div>
-        <div className="shape8 rotateme">
-          <img src={require('../../images/shape8.svg')} alt="shape" />
-        </div>
-        <div className="shape9">
-          <img src={require('../../images/shape9.svg')} alt="shape" />
-        </div>
-      </section>
-    );
-  }
-}
+      <div className="shape1">
+        <img src={require('../../images/shape1.png')} alt="img" />
+      </div>
+      <div className="shape2">
+        <img src={require('../../images/shape2.png')} alt="img" />
+      </div>
+      <div className="shape3">
+        <img src={require('../../images/shape3.png')} alt="img" />
+      </div>
+      <div className="shape6">
+        <img src={require('../../images/shape6.png')} alt="img" />
+      </div>
+      <div className="shape8 rotateme">
+        <img src={require('../../images/shape8.svg')} alt="shape" />
+      </div>
+      <div className="shape9">
+        <img src={require('../../images/shape9.svg')} alt="shape" />
+      </div>
+    </section>
+  );
+};
 
 export default Banner;
--- components/ContactUs/Content.js
+++ components/ContactUs/Content.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 
 class Content extends Component {
   render() {
@@ -9,41 +7,27 @@ class Content extends Component {
       <section className="contact-info-area ptb-100">
         <div className="container">
           <div className="row">
-            <div className="col-lg-4 col-md-6">
+            <div className="col-lg-6 col-md-6">
               <div className="contact-info-box">
                 <div className="icon">
-                  <FontAwesomeIcon icon={["fas","at"]}/>
+                  <FontAwesomeIcon icon={['fas', 'at']} />
                 </div>
                 <h3>Email</h3>
                 <p>
-                  <Link href="#">
-                    <a>dscvjitclub@gmail.com</a>
-                  </Link>
+                  <a href="mailto:dscvjitclub@gmail.com">
+                    dscvjitclub@gmail.com
+                  </a>
                 </p>
               </div>
             </div>
 
-            <div className="col-lg-4 col-md-6">
+            <div className="col-lg-6 col-md-6">
               <div className="contact-info-box">
                 <div className="icon">
-                  <FontAwesomeIcon icon={["fas","map-marker-alt"]}/>
+                  <FontAwesomeIcon icon={['fas', 'map-marker-alt']} />
                 </div>
                 <h3>Address</h3>
                 <p>Aziz Nagar, VJIT</p>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 offset-lg-0 offset-md-3">
-              <div className="contact-info-box">
-                <div className="icon">
-                  <FontAwesomeIcon icon={["fas","phone-alt"]}/>
-                </div>
-                <h3>Phone Number</h3>
-                <p>
-                  <Link href="#">
-                    <a>+91 8328277518</a>
-                  </Link>
-                </p>
               </div>
             </div>
           </div>
