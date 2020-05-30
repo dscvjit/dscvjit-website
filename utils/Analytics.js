89ea084aad409e44672bd31dcdec1e909078f9f9
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-05-30 17:14:19 +0530

    Added the google analytics

---
+++ utils/Analytics.js
@@ -0,0 +1,20 @@
+import ReactGA from 'react-ga'
+
+export const initGA = () => {
+  ReactGA.initialize('UA-125812590-3')
+}
+export const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+}
+export const logEvent = (category = '', action = '') => {
+  if (category && action) {
+    ReactGA.event({ category, action })
+  }
+}
+
+export const logException = (description = '', fatal = false) => {
+  if (description) {
+    ReactGA.exception({ description, fatal })
+  }
+}
