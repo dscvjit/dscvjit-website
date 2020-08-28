91884c169681f88280657eb975269a5ab1669adc
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-08-29 00:21:28 +0530

    Added the env support

---
+++ config/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+//place the config here
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+export const db = firebase.firestore();
+export default firebase;
--- utils/Analytics.js
+++ utils/Analytics.js
@@ -1,20 +1,20 @@
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  ReactGA.initialize('UA-168037084-1')
-}
+  ReactGA.initialize(process.env.REACT_APP_TRACKING_CODE);
+};
 export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname })
-  ReactGA.pageview(window.location.pathname)
-}
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
 export const logEvent = (category = '', action = '') => {
   if (category && action) {
-    ReactGA.event({ category, action })
+    ReactGA.event({ category, action });
   }
-}
+};
 
 export const logException = (description = '', fatal = false) => {
   if (description) {
-    ReactGA.exception({ description, fatal })
+    ReactGA.exception({ description, fatal });
   }
-}
+};
