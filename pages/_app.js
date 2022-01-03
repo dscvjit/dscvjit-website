7fa50ccc00effbda0ae200ef25d00efd61ac0939
Ashfaq <ashfaqnisar00@gmail.com>
2022-01-03 17:49:15 +0530

    fix: Updated the styles of the application

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,36 @@
-import '../assets/css/bootstrap.min.css';
+import '../styles/bootstrap.min.css';
+import '../styles/style.scss';
+import '../styles/responsive.scss';
+import '../styles/animate.css';
 import 'react-accessible-accordion/dist/fancy-example.css';
-import '../assets/css/style.scss';
-import '../assets/css/responsive.scss';
-import '../assets/css/animate.css';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-
 import { Preloader, Placeholder } from 'react-preloading-screen';
-import App from 'next/app';
-import { DefaultSeo } from 'next-seo';
-import GoTop from '../components/Shared/GoTop';
-import Loader from '../components/Shared/Loader';
-import React from 'react';
+import { GoTop, Loader } from '../components/shared';
+import { NextSeo } from 'next-seo';
 import '../fontAwesome';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps, store } = this.props;
-    return (
-      <>
-        <DefaultSeo
-          title="Developer Student Clubs VJIT"
-          description="The Developer Student Clubs website for Vidya Jyothi Institute of Technology"
-          openGraph={{
-            type: 'website',
-            locale: 'en_IE',
-            url: 'https://dscvjit.com',
-            site_name: 'DSC VJIT'
-          }}
-        />
-        <Preloader>
-          <Component {...pageProps} />
-          <GoTop scrollStepInPx="50" delayInMs="20" />
-          <Placeholder>
-            <Loader />
-          </Placeholder>
-        </Preloader>
-      </>
-    );
-  }
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NextSeo
+        title="Developer Student Clubs VJIT"
+        description="The Developer Student Clubs website for Vidya Jyothi Institute of Technology"
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://dscvjit.com',
+          site_name: 'DSC VJIT'
+        }}
+      />
+      <Preloader>
+        <Component {...pageProps} />
+        <GoTop scrollStepInPx="50" delayInMs="20" />
+        <Placeholder>
+          <Loader />
+        </Placeholder>
+      </Preloader>
+    </>
+  );
 }
+
 export default MyApp;
